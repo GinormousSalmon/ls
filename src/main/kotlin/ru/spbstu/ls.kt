6d0515747772b598cbfc58a7a61cce3032da06f5
GinormousSalmon <terleczcky.alex@gmail.com>
2020-08-17 21:41:55 +1000

    split into two files: LsLauncher and Ls fix reversed reversing

---
+++ src/main/kotlin/ru/spbstu/ls.kt
@@ -0,0 +1,61 @@
+package ru.spbstu
+
+import java.io.File
+import java.util.*
+
+class Ls{
+    private fun getBinMask(file: File): String {
+        return (if (file.canExecute()) "1" else "0") +
+                (if (file.canRead()) "1" else "0") +
+                (if (file.canWrite()) "1" else "0")
+    }
+
+    private fun getRwxMask(file: File): String {
+        return "%3s".format(        //If one of the flags (r,w,x) is missing, output table will remain aligned
+            (if (file.canExecute()) "x" else "") +
+                    (if (file.canWrite()) "w" else "") +
+                    (if (file.canRead()) "r" else "")
+        ).reversed()
+    }
+
+    private fun convertSize(s: Long): String {
+        val dim = listOf("B", "KB", "MB", "GB", "TB")
+        var size: Double = s.toDouble()
+        var count = 0
+        while (size > 1023) {
+            size /= 1024
+            count++
+        }
+        return "%.3f".format(size) + dim[count]
+    }
+
+    fun work(isLong: Boolean, isHumanReadable: Boolean, isReversed: Boolean, out: File?, path: File) {
+        val files = when {
+            path.isDirectory -> path.listFiles()!!.asList()
+            path.isFile -> listOf(path)
+            else -> throw IllegalArgumentException()
+        }.toMutableList()
+        val longestName = files.maxBy { it.name }!!.name.length + 5
+        if (isReversed)
+            files.sortByDescending { it.name }
+        else
+            files.sortBy { it.name }
+        val result = if (isLong) {
+            if (isHumanReadable)
+                files.map {
+                    it.name + "%${longestName - it.name.length}s".format(getRwxMask(it)) +
+                            "  ${Date(it.lastModified())}  ${convertSize(it.length())}"
+                }
+            else
+                files.map { "${it.name}, ${getBinMask(it)},  ${it.lastModified()},  ${it.length()}" }
+        } else {
+            files.map { it.name }.sorted()
+        }
+        if (out != null) {
+            val outputStream = out.bufferedWriter()
+            outputStream.write(result.joinToString("\n"))
+            outputStream.close()
+        }
+        println(result.joinToString("\n"))
+    }
+}
--- src/main/kotlin/ru/spbstu/lsLauncher.kt
+++ src/main/kotlin/ru/spbstu/lsLauncher.kt
@@ -1,11 +1,7 @@
 package ru.spbstu
 
-import org.kohsuke.args4j.Argument
-import org.kohsuke.args4j.CmdLineException
-import org.kohsuke.args4j.CmdLineParser
-import org.kohsuke.args4j.Option
+import org.kohsuke.args4j.*
 import java.io.File
-import java.util.*
 
 fun main(args: Array<String>) {
     LsLauncher().launch(args)
@@ -27,31 +23,6 @@ class LsLauncher {
     @Argument(required = true, metaVar = "Directory path", usage = "Analysed directory")
     private lateinit var path: File
 
-    private fun getBinMask(file: File): String {
-        return (if (file.canExecute()) "1" else "0") +
-                (if (file.canRead()) "1" else "0") +
-                (if (file.canWrite()) "1" else "0")
-    }
-
-    private fun getRwxMask(file: File): String {
-        return "%3s".format(        //If one of the flags (r,w,x) is missing, output table will remain aligned
-            (if (file.canExecute()) "x" else "") +
-                    (if (file.canWrite()) "w" else "") +
-                    (if (file.canRead()) "r" else "")
-        ).reversed()
-    }
-
-    private fun convertSize(s: Long): String {
-        val dim = listOf("B", "KB", "MB", "GB", "TB")
-        var size: Double = s.toDouble()
-        var count = 0
-        while (size > 1023) {
-            size /= 1024
-            count++
-        }
-        return "%.3f".format(size) + dim[count]
-    }
-
     fun launch(args: Array<String>) {
         val parser = CmdLineParser(this)
         try {
@@ -60,32 +31,6 @@ class LsLauncher {
             println(e.message)
             parser.printUsage(System.out)
         }
-        val files = when {
-            path.isDirectory -> path.listFiles()!!.asList()
-            path.isFile -> listOf(path)
-            else -> throw IllegalArgumentException()
-        }.toMutableList()
-        val longestName = files.maxBy { it.name }!!.name.length + 5
-        if (isReversed)
-            files.sortBy { it.name }
-        else
-            files.sortByDescending { it.name }
-        val result = if (isLong) {
-            if (isHumanReadable)
-                files.map {
-                    it.name + "%${longestName - it.name.length}s".format(getRwxMask(it)) +
-                            "  ${Date(it.lastModified())}  ${convertSize(it.length())}"
-                }
-            else
-                files.map { "${it.name}, ${getBinMask(it)},  ${it.lastModified()},  ${it.length()}" }
-        } else {
-            files.map { it.name }.sorted()
-        }
-        if (out != null) {
-            val outputStream = out!!.bufferedWriter()
-            outputStream.write(result.joinToString("\n"))
-            outputStream.close()
-        }
-        println(result.joinToString("\n"))
+        Ls().work(isLong, isHumanReadable, isReversed, out, path)
     }
 }
